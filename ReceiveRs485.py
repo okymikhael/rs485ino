7e86603aac0813cbd1b1e83fa228f8d83956341b
okymikhael <okymikhael@gmail.com>
2020-05-11 08:43:38 +0700

    Add files via upload

---
+++ ReceiveRs485.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import time
+import serial
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT, initial= GPIO.LOW)
+
+ser1 = serial.Serial('/dev/ttyUSB1',9600)
+                   
+Ser1.flushInput()
+
+while True:
+    if Ser1.inWaiting():
+        mdata = Ser1.read().decode()
+        if mdata = Ser1.readline().decode().strip()
+        mdata = mdata.split('#')
+        sensor = int (mdata[1])
+        print ("ID : {}, Intensitas : {}".
+               format (mdata[0], sensor))
+
+ser1.close()
+    
---
+++ send_Arduino_RS485.ino
@@ -0,0 +1,33 @@
+#include <SoftwareSerial.h>
+
+int ldr;
+int EN = 2;
+int intensitas;
+
+SoftwareSerial mySerial(6,7);  //rx, tx
+
+void setup() 
+{
+ pinMode(EN, OUTPUT);
+ Serial.begin (9600);
+ mySerial.begin(9600);
+ digitalWrite(EN,LOW);
+
+}//--(end setup )---
+
+void loop()    
+{
+ digitalWrite(EN, HIGH); // enable send
+ delay(5);
+ // Send Data
+ ldr = analogRead(A0);
+ //kalibrasi sesuai luxmeter
+ intensitas = ((pow(ldr,-1.71))*(pow(10,8.14)));
+ Serial.print("$01#");
+ Serial.print(intensitas);
+ Serial.print("\n");
+ mySerial.print("$01#");
+ mySerial.print(intensitas);
+ mySerial.print("\n");
+ delay(1000);
+}
