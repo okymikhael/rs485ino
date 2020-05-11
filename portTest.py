3a3c43dc82273336bfc13208c9d76b8b31738e0e
Oky Mikhael <okymikhael@gmail.com>
2020-05-11 10:15:54 +0700

    looping port

--- portTest.py
+++ portTest.py
@@ -17,12 +17,8 @@ send = serial.Serial(
 
 i = [0,10,45,90,135,180,135,90,45,10,0]
 
-# while True:
-#      for x in i:
-#         send.write(str(x))
-#         print(x)
-
-mdata = send.read().decode()
-print(mdata)
-print(send.read())
-time.sleep(1.5)
+while True:
+    mdata = send.read().decode()
+    print(mdata)
+    print(send.read())
+    time.sleep(1.5)
