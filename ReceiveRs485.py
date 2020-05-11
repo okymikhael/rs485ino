9d4d5a2fa8ff0e49ea7af3d259e322d83f4061bd
Oky Mikhael <okymikhael@gmail.com>
2020-05-11 09:55:52 +0700

    add documentation

--- ReceiveRs485.py
+++ ReceiveRs485.py
@@ -6,7 +6,7 @@ from time import sleep
 
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT, initial= GPIO.LOW)
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
 
 Ser1 = serial.Serial('/dev/ttyUSB0',9600)
                    
@@ -17,6 +17,7 @@ while True:
     if Ser1.inWaiting():
         mdata = Ser1.read().decode()
         print(mdata)
+        print("Masuk")
 
         if mdata == Ser1.readline().decode().strip():
             mdata = mdata.split('#')
---
+++ portTest.py
@@ -0,0 +1,24 @@
+import time
+import serial
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT, initial=GPIO.HIGH)
+
+send = serial.Serial(
+    port='/dev/serial0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+i = [0,10,45,90,135,180,135,90,45,10,0]
+
+while True:
+     for x in i:
+        send.write(str(x))
+        print(x)
+        time.sleep(1.5)
