e483001fa412c8abcb2f13765e05e1cf78aaba2e
Oky Mikhael <okymikhael@gmail.com>
2020-05-11 09:13:22 +0700

    another print

--- ReceiveRs485.py
+++ ReceiveRs485.py
@@ -15,13 +15,13 @@ Ser1.flushInput()
 while True:
     print("Waiting")
     if Ser1.inWaiting():
-        print("Masuk")
         mdata = Ser1.read().decode()
+        print(mdata)
 
         if mdata == Ser1.readline().decode().strip():
             mdata = mdata.split('#')
             sensor = int (mdata[1])
             print ("ID : {}, Intensitas : {}".format (mdata[0], sensor))
-            
+
     sleep(1)
 # Ser1.close()
