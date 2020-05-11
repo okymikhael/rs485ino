51b6e8c2bb2beb879ab02104dc89e4662e83abb6
Oky Mikhael <okymikhael@gmail.com>
2020-05-11 08:50:17 +0700

    repair syntax

--- ReceiveRs485.py
+++ ReceiveRs485.py
@@ -8,18 +8,17 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT, initial= GPIO.LOW)
 
-ser1 = serial.Serial('/dev/ttyUSB1',9600)
+Ser1 = serial.Serial('/dev/ttyUSB1',9600)
                    
 Ser1.flushInput()
 
 while True:
     if Ser1.inWaiting():
         mdata = Ser1.read().decode()
-        if mdata = Ser1.readline().decode().strip()
-        mdata = mdata.split('#')
-        sensor = int (mdata[1])
-        print ("ID : {}, Intensitas : {}".
-               format (mdata[0], sensor))
 
-ser1.close()
-    
+        if mdata == Ser1.readline().decode().strip():
+            mdata = mdata.split('#')
+            sensor = int (mdata[1])
+            print ("ID : {}, Intensitas : {}".format (mdata[0], sensor))
+
+Ser1.close()
