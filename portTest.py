cb584c15456901f0deca93370287f2d9a12fe0fc
Oky Mikhael <okymikhael@gmail.com>
2020-05-11 10:19:02 +0700

    eadline

--- portTest.py
+++ portTest.py
@@ -12,19 +12,17 @@ send = serial.Serial(
     # parity=serial.PARITY_NONE,
     # stopbits=serial.STOPBITS_ONE,
     # bytesize=serial.EIGHTBITS,
-    timeout=1
+    timeout=10
 )
-
-i = [0,10,45,90,135,180,135,90,45,10,0]
 
 while True:
     mdata = send.read().decode()
     print(mdata)
-    print(send.read())
+    print(send.readline())
     
-    if mdata == send.readline().decode().strip():
-            mdata = mdata.split('#')
-            sensor = int (mdata[1])
-            print ("ID : {}, Intensitas : {}".format (mdata[0], sensor))
+    # if mdata == send.readline().decode().strip():
+    #     mdata = mdata.split('#')
+    #     sensor = int (mdata[1])
+    #     print ("ID : {}, Intensitas : {}".format (mdata[0], sensor))
 
     time.sleep(1.5)
